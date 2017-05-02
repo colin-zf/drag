42a264f6f31025cb0f88f79f9e7e58c4578f0e70
colin-zf <1494560176@qq.com>
2017-05-02 14:33:27 +0800

    sbumit drag1.html and drag.js from master branch

--- drag.js
+++ drag.js
@@ -1,19 +1,40 @@
 function fnDrag(id){
 	var oDiv=document.getElementById(id);
-	
 	var disX=0;
 	var disY=0;
 	
 	oDiv.onmousedown=function(ev){
 		var ev=ev||event;
-		disX=ev.pageX-oDiv.offsetLeft;
-		disY=ev.pageY-oDiv.offsetTop;
+		disX=ev.clientX-oDiv.offsetLeft;
+		disY=ev.clientY-oDiv.offsetTop;
 		
 		document.onmousemove=function(ev){
 			var ev=ev||event;
-			oDiv.style.left=ev.pageX-disX+"px";
-			oDiv.style.top=ev.pageY-disY+"px";
-
+			oDiv.style.left=ev.clientX-disX+"px";
+			oDiv.style.top=ev.clientY-disY+"px";
+			
+			if(oDiv.offsetLeft<0){
+				oDiv.style.left=0+"px";
+			}else if(oDiv.offsetLeft>=ev.clientX-oDiv.offsetWidth){
+				oDiv.style.left=ev.clientX-oDiv.offsetWidth+"px";
+			}else{
+				oDiv.style.left=ev.clientX-disX+"px";
+			}
+			
+			if(oDiv.offsetTop<0){
+				oDiv.style.top=0+"px";
+			}else if(oDiv.offsetTop>=ev.clientY-oDiv.offsetHeight){
+				oDiv.style.top=ev.clientY-oDiv.offsetHeight+"px";
+			}else{
+				oDiv.style.top=ev.clientY-disY+"px";
+			}
+		 
+		}
+		
+		document.onmouseup=function(){
+			document.onmousemove=null;
+			document.onmouseup=null;
 		}
 	};
+	return false;
 }
